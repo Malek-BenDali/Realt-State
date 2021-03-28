a26d8d412fd5d8844dc6288ddf5f415b84745a9b
Malek Ben Dali <bendali.malekk@gmail.com>
2021-03-28 15:12:53 +0100

    prettier

--- front/src/components/Listings.js
+++ front/src/components/Listings.js
@@ -1,58 +1,52 @@
-import React from 'react';
-import Card from './Card';
+import React from 'react'
+import Card from './Card'
 
 const listings = ({ listings }) => {
-    const getListings = () => {
-        let listingsOnPage = [];
-        let result = [];
+	const getListings = () => {
+		let listingsOnPage = []
+		let result = []
 
-        if (listingsOnPage.length > 0){
-            listings.map(listing => {
-                return listingsOnPage.push(
-                    <Card
-                        title={listing.title}
-                        address={listing.address}
-                        city={listing.city}
-                        state={listing.state}
-                        price={listing.price}
-                        sale_type={listing.sale_type}
-                        home_type={listing.home_type}
-                        bedrooms={listing.bedrooms}
-                        bathrooms={listing.bathrooms}
-                        sqft={listing.sqft}
-                        photo_main={listing.photo_main}
-                        slug={listing.slug}
-                    />
-                );
-            });
-        }
+		if (listingsOnPage.length > 0) {
+			listings.map(listing => {
+				return listingsOnPage.push(
+					<Card
+						title={listing.title}
+						address={listing.address}
+						city={listing.city}
+						state={listing.state}
+						price={listing.price}
+						sale_type={listing.sale_type}
+						home_type={listing.home_type}
+						bedrooms={listing.bedrooms}
+						bathrooms={listing.bathrooms}
+						sqft={listing.sqft}
+						photo_main={listing.photo_main}
+						slug={listing.slug}
+					/>
+				)
+			})
+		}
 
-        if( listings){
-            for (let i = 0; i < listings.length; i += 3) {
-                result.push(
-                    <div className='row'>
-                        <div className='col-1-of-3'>
-                            {listingsOnPage[i]}
-                        </div>
-                        <div className='col-1-of-3'>
-                        {listingsOnPage[i+1] ? listingsOnPage[i+1] : null}
-                        </div>
-                        <div className='col-1-of-3'>
-                        {listingsOnPage[i+2] ? listingsOnPage[i+2] : null}
-                        </div>
-                    </div>
-                );
-            }
-    
-            return result;
-        };
-        }
+		if (listings) {
+			for (let i = 0; i < listings.length; i += 3) {
+				result.push(
+					<div className="row">
+						<div className="col-1-of-3">{listingsOnPage[i]}</div>
+						<div className="col-1-of-3">
+							{listingsOnPage[i + 1] ? listingsOnPage[i + 1] : null}
+						</div>
+						<div className="col-1-of-3">
+							{listingsOnPage[i + 2] ? listingsOnPage[i + 2] : null}
+						</div>
+					</div>
+				)
+			}
 
-    return (
-        <div>
-            {getListings()}
-        </div>
-    );
+			return result
+		}
+	}
+
+	return <div>{getListings()}</div>
 }
 
-export default listings;
+export default listings
--- front/src/components/Navbar.js
+++ front/src/components/Navbar.js
@@ -1,57 +1,76 @@
 import React from 'react'
-import { Link, NavLink } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom'
 import { connect } from 'react-redux'
 import Alert from './Alert'
-import PropTypes from 'prop-types';
+import PropTypes from 'prop-types'
 
-function Navbar({auth : {token, loading} }) {
-    const authLinks = (
-        <>
-        <Link className='navbar__top__auth__link' to='/logout'>Deconnexion</Link>
-        </>
-    );
+function Navbar({ auth: { token, loading } }) {
+	const authLinks = (
+		<>
+			<Link className="navbar__top__auth__link" to="/logout">
+				Deconnexion
+			</Link>
+		</>
+	)
 
-    const guestLinks =(
-        <>
-            <Link className='navbar__top__auth__link' to='/login'>Connexion</Link>
-            <Link className='navbar__top__auth__link' to='/signup'>Inscription</Link>
-        </>
-    )
+	const guestLinks = (
+		<>
+			<Link className="navbar__top__auth__link" to="/login">
+				Connexion
+			</Link>
+			<Link className="navbar__top__auth__link" to="/signup">
+				Inscription
+			</Link>
+		</>
+	)
 
-    return (
-        <>
-            <nav className='navbar'>
-                <div className="navbar__top">
-                    <div className='navbar__top__logo'>
-                        <Link className='navbar__top__logo__link' to='/listings'> Logement </Link>
-                    </div>
-                    <div className='navbar__top__auth'>
-                        { !loading && (<> {token ? authLinks : guestLinks} </>)}
-                    </div>
-                </div>
-                <div className='navbar__bottom' >
-                    <li className='navbar__bottom__item'>
-                        <NavLink className='navbar__bottom__item__link' exact to='/listings'>Liste des logements</NavLink>
-                    </li>
-                    <li className='navbar__bottom__item'>
-                        <NavLink className='navbar__bottom__item__link' exact to='/about'>A propos</NavLink>
-                    </li>
-                    <li className='navbar__bottom__item'>
-                        <NavLink className='navbar__bottom__item__link' exact to='/contact'>Contact</NavLink>
-                    </li>
-                </div>
-                <Alert />
-            </nav>
-        </>
-    )
+	return (
+		<>
+			<nav className="navbar">
+				<div className="navbar__top">
+					<div className="navbar__top__logo">
+						<Link className="navbar__top__logo__link" to="/listings">
+							{' '}
+							Logement{' '}
+						</Link>
+					</div>
+					<div className="navbar__top__auth">
+						{!loading && <> {token ? authLinks : guestLinks} </>}
+					</div>
+				</div>
+				<div className="navbar__bottom">
+					<li className="navbar__bottom__item">
+						<NavLink
+							className="navbar__bottom__item__link"
+							exact
+							to="/listings"
+						>
+							Liste des logements
+						</NavLink>
+					</li>
+					<li className="navbar__bottom__item">
+						<NavLink className="navbar__bottom__item__link" exact to="/about">
+							A propos
+						</NavLink>
+					</li>
+					<li className="navbar__bottom__item">
+						<NavLink className="navbar__bottom__item__link" exact to="/contact">
+							Contact
+						</NavLink>
+					</li>
+				</div>
+				<Alert />
+			</nav>
+		</>
+	)
 }
 
-Navbar.propTypes={
-    auth : PropTypes.object.isRequired
+Navbar.propTypes = {
+	auth: PropTypes.object.isRequired,
 }
 
-const mapStateToProps = state =>({
-    auth : state.auth
+const mapStateToProps = state => ({
+	auth: state.auth,
 })
 
-export default connect(mapStateToProps)(Navbar);
+export default connect(mapStateToProps)(Navbar)
